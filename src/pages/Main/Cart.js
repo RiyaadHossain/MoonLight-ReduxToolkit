b9711bf91a131c0116fe84185f066b3364e54de2
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-12-15 21:09:34 +0600

    starter pack added

---
+++ src/pages/Main/Cart.js
@@ -0,0 +1,13 @@
+import React from "react";
+// import ProductCard from "../../components/ProductCard";
+
+const Cart = () => {
+  
+  return (
+    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
+      {}
+    </div>
+  );
+};
+
+export default Cart;
